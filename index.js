b6cb1b613ee5c0f92d3d5a401deb9045b354c909
Dinesh-0212 <123875808+Dinesh-0212@users.noreply.github.com>
2025-05-08 10:33:25 +0530

    index.js

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ function convertion(val)
 
 }
 
-btn.addEventListener('click',function()
-{
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
+btn.addEventListener('click', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik)
     .then(res => res.json())
+    .then(data => {
+        if (data.cod == "404") {
+            alert('You entered wrong city');
+            return;
+        }
 
-    .then(data =>
-        {
-            var nameval=data['name']
-            var descrip = data['weather']['0']['description']
-            var temperature = data['main']['temp']
-            var wndspeed = data['wind']['speed']
+        var nameval = data['name'];
+        var descrip = data['weather'][0]['description'];
+        var temperature = data['main']['temp'];
+        var wndspeed = data['wind']['speed'];
 
-            city.innerHTML = `Weather of <span> ${nameval}<span>`
-            temp.innerHTML = `Temperature of <span>${convertion(temperature)} C <span>`
-            description.innerHTML= `Sky Conditions : <span> ${descrip} <span>`
-            wind.innerHTML = `Wind Speed : <span>${wndspeed} km/h<span>`
-
-        })
-
-        .catch(err => alert('You entered wrong city'))
-})
+        city.innerHTML = `Weather of <span> ${nameval}</span>`;
+        temp.innerHTML = `Temperature of <span>${convertion(temperature)} C</span>`;
+        description.innerHTML = `Sky Conditions : <span> ${descrip}</span>`;
+        wind.innerHTML = `Wind Speed : <span>${wndspeed} km/h</span>`;
+    })
+    .catch(err => alert('Error fetching weather data!'));
+});
